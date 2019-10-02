6edf53a27f66a7739c3254e0c3af37c07be24369
Stian Ådnanes <stian@dhcp-10-24-74-247.wlan.ntnu.no>
2019-10-02 12:42:58 +0200

    fixed server

--- Oving6/SocketServer/app/src/main/java/leksjon/socket/Server.java
+++ Oving6/SocketServer/app/src/main/java/leksjon/socket/Server.java
@@ -18,37 +18,39 @@ public class Server extends Thread{
 		Socket s 			= null;
 		PrintWriter out 	= null;
 		BufferedReader in 	= null;
-		
-		try{
-    		Log.i(TAG,"start server....");
-            ss = new ServerSocket(PORT);
-            Log.i(TAG,"serversocket created, wait for client....");
-            s = ss.accept();
-            Log.v(TAG, "client connected...");
-            out = new PrintWriter(s.getOutputStream(), true);
-            in = new BufferedReader(
-            		new InputStreamReader(s.getInputStream()));
+		do {
+			try {
+				Log.i(TAG, "start server....");
+				ss = new ServerSocket(PORT);
+				Log.i(TAG, "serversocket created, wait for client....");
+				s = ss.accept();
+				Log.v(TAG, "client connected...");
+				out = new PrintWriter(s.getOutputStream(), true);
+				in = new BufferedReader(
+						new InputStreamReader(s.getInputStream()));
 
-            out.println("Welcome client...");//send text to client
-            
-            String res = in.readLine();//receive text from client
-			String[] split = res.split(" ");
-			for(int i =0; i<split.length;i++) {
-				Log.i(TAG,"Message from client: " + split[i]);
+				out.println("Welcome client...");//send text to client
+
+				String res = in.readLine();//receive text from client
+				String[] split = res.split(" ");
+				for (int i = 0; i < split.length; i++) {
+					Log.i(TAG, "Message from client: " + split[i]);
+				}
+				int tall1 = Integer.parseInt(split[0]);
+				int tall2 = Integer.parseInt(split[1]);
+				out.println((tall1 + tall2));
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {//close sockets!!
+				try {
+					out.close();
+					in.close();
+					s.close();
+					ss.close();
+				} catch (Exception e) {
+				}
 			}
-			int tall1 = Integer.parseInt(split[0]);
-			int tall2 = Integer.parseInt(split[1]);
-			out.println((tall1+tall2));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally{//close sockets!!
-        	try{
-        		out.close();
-            	in.close();
-        		s.close();
-        		ss.close();
-        	}catch(Exception e){}	            	
-        }   
+		}while(true);
 	}
 }
